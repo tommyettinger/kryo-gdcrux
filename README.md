b487b0d8d8982fb4339c042682e78a44d7590e67
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-23 20:43:38 -0700

    Bring over kryo-more code.

---
+++ README.md
@@ -0,0 +1,19 @@
+# kryo-gdcrux
+Kryo 5.x support for classes from [gdcrux](https://github.com/tommyettinger/gdcrux)
+
+## Get it
+
+Use JitPack.io as your repository (which gdx-liftoff projects already can do) and depend on:
+```
+implementation "com.github.tommyettinger:kryo-gdcrux:0.1.2.2"
+```
+
+This depends on Kryo 5.6.2 and libGDX 1.13.1 (or newer), as well as gdcrux 0.1.2 and crux 0.1.3 .
+
+## Older Versions
+
+See [kryo-more](https://github.com/tommyettinger/kryo-more) for older releases.
+
+## License
+
+[Apache 2.0](LICENSE).
---
+++ src/main/java/com/github/tommyettinger/kryo/gdcrux/PointF6Serializer.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2022-2024 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.github.tommyettinger.kryo.gdcrux;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.github.tommyettinger.gdcrux.PointF6;
+
+/**
+ * Kryo {@link Serializer} for gdcrux {@link PointF6}s.
+ */
+public class PointF6Serializer extends Serializer<PointF6> {
+
+    public PointF6Serializer() {
+        setAcceptsNull(false);
+        setImmutable(false);
+    }
+
+    @Override
+    public void write(final Kryo kryo, final Output output, final PointF6 data) {
+        output.writeFloat(data.x);
+        output.writeFloat(data.y);
+        output.writeFloat(data.z);
+        output.writeFloat(data.w);
+        output.writeFloat(data.u);
+        output.writeFloat(data.v);
+    }
+
+    @Override
+    public PointF6 read(final Kryo kryo, final Input input, final Class<? extends PointF6> dataClass) {
+        return new PointF6(input.readFloat(), input.readFloat(), input.readFloat(),
+                input.readFloat(), input.readFloat(), input.readFloat());
+    }
+
+    @Override
+    public PointF6 copy(Kryo kryo, PointF6 original) {
+        return new PointF6(original);
+    }
+}
